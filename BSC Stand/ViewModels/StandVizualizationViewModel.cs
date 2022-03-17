b02fb7fc6fedb0445262a21baf6777a209a030bd
Денис Черный <userden4ikwork@gmail.com>
2022-03-17 23:56:59 +0300

    OxyPlotSampleAdd

--- BSC Stand/ViewModels/StandVizualizationViewModel.cs
+++ BSC Stand/ViewModels/StandVizualizationViewModel.cs
@@ -20,10 +20,11 @@ namespace BSC_Stand.ViewModels
             
             var verticalAxis = new LinearAxis { Position = AxisPosition.Left,  Minimum = 0, Maximum = 1000 };
             
-            var horizontalAxis = new LinearAxis { Position = AxisPosition.Right };
+            var horizontalAxis = new LinearAxis { Position = AxisPosition.Bottom, Maximum = 1000};
             testPlotModel.Axes.Add(verticalAxis);
-            testPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
-            testPlotModel.Series.Add(new FunctionSeries(x => Math.Sin(x * Math.PI * 4) * Math.Sin(x * Math.PI * 4) * Math.Sqrt(x) * 1000, 0, 1, 1000));
+
+            testPlotModel.Axes.Add(horizontalAxis);
+            testPlotModel.Series.Add(new FunctionSeries(x => Math.Sin(x * Math.PI * 4) * Math.Sin(x * Math.PI * 4) * Math.Sqrt(x) * 100, 0, 1000, 100));
         }
 
     }
