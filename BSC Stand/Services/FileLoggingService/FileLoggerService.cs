fb8fbe5d3caf298956c0a68ea59230ff9842a6f0
Денис Черный <userden4ikwork@gmail.com>
2022-06-15 16:43:49 +0300

    StreamWriter

--- BSC Stand/Services/FileLoggingService/FileLoggerService.cs
+++ BSC Stand/Services/FileLoggingService/FileLoggerService.cs
@@ -16,44 +16,46 @@ namespace BSC_Stand.Services
     {
 
         private string FilePath;
-
+        private StreamWriter StreamWriter;
 
         public void CreateFile()
         {
             FilePath = $@"{Environment.CurrentDirectory}/Файлы пользователя/Записи экспериментов/{DateTime.Now.ToFileTime()}.csv";
-            File.CreateText(FilePath).Close();
+            StreamWriter = File.CreateText(FilePath);
+         
             
         }
 
         public void WriteLog(ReadingParams readingParams)
         {
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                // Don't write the header again
-                HasHeaderRecord = false,
-            };
+
+            StreamWriter.WriteLine("readingParams");
 
 
-            using (var stream = File.Open(FilePath, FileMode.Append))
-            {
-                using (var writer = new StreamWriter("FilePath"))
-                {
-                    using (var csv = new CsvWriter(writer, config))
-                    {
+            //var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            //{
+            //    // Don't write the header again
+            //    HasHeaderRecord = false,
+            //};
+
+
+            //using (var stream = File.Open(FilePath, FileMode.Append))
+            //{
+            //    using (var writer = new StreamWriter("FilePath"))
+            //    {
+            //        using (var csv = new CsvWriter(writer, config))
+            //        {
                       
-                        csv.WriteHeader<ReadingParams>();
-                        csv.WriteRecord(readingParams);
-                        csv.WriteComment("аывы");
-                        csv.NextRecord();
-                        Debug.WriteLine(readingParams.AKIPAValue);
-                        csv.Flush();
-                        writer.AutoFlush = true;
-                        stream.Flush();
+            //            csv.WriteHeader<ReadingParams>();
+            //            csv.WriteRecord(readingParams);
+            //            csv.WriteComment("аывы");
+            //            csv.NextRecord();
+            //            Debug.WriteLine(readingParams.AKIPAValue);
+                      
+            //        }
 
-                    }
-
-                }
-            }
+            //    }
+            //}
 
         }
 
