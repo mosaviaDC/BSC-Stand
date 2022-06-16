5b8ca584586075a43970c1a36813970db32dd87a
Денис Черный <userden4ikwork@gmail.com>
2022-06-16 16:46:42 +0300

    Дата и время как название файла

--- BSC Stand/Services/FileLoggingService/FileLoggerService.cs
+++ BSC Stand/Services/FileLoggingService/FileLoggerService.cs
@@ -19,7 +19,7 @@ namespace BSC_Stand.Services
 
         public void CreateFile()
         {
-            FilePath = $@"{Environment.CurrentDirectory}/Файлы пользователя/Отчеты/CSV/{DateTime.Now.Date}{DateTime.Now.Hour}{DateTime.Now.Minute}.csv";
+            FilePath = $@"{Environment.CurrentDirectory}/Файлы пользователя/Отчеты/CSV/Отчет от {DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} {DateTime.Now.Hour}.{DateTime.Now.Minute}.csv";
             File.CreateText(FilePath).Close();
             using (var stream = File.Open(FilePath, FileMode.Append))
             using (var writer = new StreamWriter(stream))
