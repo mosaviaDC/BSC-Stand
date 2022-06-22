dd02a543699beba71abae5a77a7b899da67d1f7e
Денис Черный <userden4ikwork@gmail.com>
2022-06-22 16:09:38 +0300

    д

---
+++ BSC Stand/Models/StandConfigurationModels/ElectronicLoads/ElectronicLoad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSC_Stand.Models.StandConfigurationModels
+{
+    internal class ElectronicLoad:ProggramebleModule
+    {
+        public List<ElectronicConfigMode> Modes { get; }
+        public ConfigMode CurrentConfigMode { get; set; }
+        public string ModuleName { get; }
+        public ElectronicLoad(string Name,List<ElectronicConfigMode> configurationModes)
+        {
+            this.ModuleName = Name;
+            this.Modes = configurationModes;
+
+        }
+    }
+}
---
+++ BSC Stand/Models/StandConfigurationModels/PowerSupply/PowerSupplyConfigMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSC_Stand.Models.StandConfigurationModels
+{
+    internal class PowerSupplyConfigMode:ConfigMode
+    {
+        public int MaxValue1 { get; set; }
+
+    }
+}
